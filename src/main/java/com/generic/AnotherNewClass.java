445741842c7fc69c97cb8ef59e0fc761ebd39c5a
unknown <rezwanislam857@gmail.com>
2023-11-05 03:48:26 -0500

    Added AnotherNewClass in com.generic

---
+++ src/main/java/com/generic/AnotherNewClass.java
@@ -0,0 +1,13 @@
+package com.generic;
+
+public class AnotherNewClass {
+	
+	public void getAnotherNewClass() {
+		System.out.println("Mainul");
+	}
+	public static void main(String[] args) {
+		AnotherNewClass obj = new AnotherNewClass();
+		obj.getAnotherNewClass();
+	}
+
+}
